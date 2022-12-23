3ae0a6f2686d8e8a22a6843639e7a910a2a0987e
GianeEngel <118388626+GianeEngel@users.noreply.github.com>
2022-12-23 11:27:14 -0300

    Update preditorPN.py

--- preditorPN.py
+++ preditorPN.py
@@ -11,9 +11,9 @@ import joblib
 classes = ['COVID-19 negative','COVID-19 positive']
 st.title('AI-Blood - COVID-19 Diagnosis')
 modelo = joblib.load('giane-pn.sav')
-mono = st.number_input('Monocytes (/10^9/L)')
-linf = st.number_input('Lymphocytes (/10^9/L)')
-plaq = st.number_input('Platelets (/10^9/L)')
+mono = st.number_input('Monocytes (10^9/L)')
+linf = st.number_input('Lymphocytes (10^9/L)')
+plaq = st.number_input('Platelets (10^9/L)')
 pcr = st.number_input('CRP (mg/dL)')
 ferr = st.number_input('Ferritin (μg/mL)')
 pac = [mono,linf,plaq,pcr,ferr]
